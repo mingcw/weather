7c80c51fe16a9c0c3fecb7a6f43030c6f6964bb5
mingcw <mingc2016@126.com>
2018-08-22 18:58:18 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mingcw\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Mingcw\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
